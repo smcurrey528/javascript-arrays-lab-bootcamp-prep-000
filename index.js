2e09725a5948cf3de80b1be69b42f3000947dfbb
Sarah Currey <curreysarah28@gmail.com>
2018-04-15 16:05:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,23 +14,21 @@ return kittens;
 function destructivelyRemoveFirstKitten() {
 kittens.shift();
 return kittens;
-+}
-+
-+function appendKitten(name){
-+  return [...kittens, name];
-+}
-+
-+function prependKitten(name) {
-+  return [name, ...kittens];
-+}
-+
-+function removeLastKitten() {
-+  return kittens.slice(0, kittens.length - 1);
-+}
-+
-+function removeFirstKitten() {
-+  return kittens.slice(1);
-+}
+}
+
+function appendKitten(name){
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+}
     
-0  package.json 100644 → 100755
-No changes.
